cbb9c7fe53e2fa5af7076716bee167cb0730ecfe
Mauricio Uehara <mauricio.uehara@piposaude.com.br>
2024-03-07 15:35:13 -0300

    config pt-BR synthesizer and transcriber

--- apps/telephony_app/outbound_call.py
+++ apps/telephony_app/outbound_call.py
@@ -1,4 +1,5 @@
 import os
+import vocode
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,20 +9,54 @@ from vocode.streaming.telephony.config_manager.redis_config_manager import (
     RedisConfigManager,
 )
 
-from speller_agent import SpellerAgentConfig
+from vocode.streaming.models.agent import ChatGPTAgentConfig
+from vocode.streaming.models.synthesizer import AzureSynthesizerConfig
+from vocode.streaming.models.message import BaseMessage
+from vocode.streaming.telephony.constants import (
+    DEFAULT_AUDIO_ENCODING,
+    DEFAULT_SAMPLING_RATE,
+    DEFAULT_CHUNK_SIZE,
+)
+from vocode.streaming.models.transcriber import (
+    AzureTranscriberConfig,
+    PunctuationEndpointingConfig,
+)
+
 
 BASE_URL = os.environ["BASE_URL"]
+
 
 
 async def main():
     config_manager = RedisConfigManager()
 
+    synthesizer_config = AzureSynthesizerConfig(
+        sampling_rate=DEFAULT_SAMPLING_RATE,
+        audio_encoding=DEFAULT_AUDIO_ENCODING,
+        voice_name="pt-BR-FranciscaNeural",
+        language_code="pt-BR",
+    )
+    transcriber_config = AzureTranscriberConfig(
+        sampling_rate=DEFAULT_SAMPLING_RATE,
+        audio_encoding=DEFAULT_AUDIO_ENCODING,
+        chunk_size=DEFAULT_CHUNK_SIZE,
+        endpointing_config=PunctuationEndpointingConfig(),
+        language="pt-BR",
+    )
+    agent_config = ChatGPTAgentConfig(
+        initial_message=BaseMessage(text="Oi tudo bem? Gostaria de agendar uma consulta para o Mauricio."),
+        prompt_preamble="Agende uma consulta médica para o Mauricio.",
+        generate_responses=True,
+    )
+
     outbound_call = OutboundCall(
         base_url=BASE_URL,
-        to_phone="+15555555555",
-        from_phone="+15555555555",
+        to_phone="+5511973567307",
+        from_phone=os.environ["OUTBOUND_CALLER_NUMBER"],
         config_manager=config_manager,
-        agent_config=SpellerAgentConfig(generate_responses=False),
+        agent_config=agent_config,
+        synthesizer_config=synthesizer_config,
+        transcriber_config=transcriber_config,
     )
 
     input("Press enter to start call...")
